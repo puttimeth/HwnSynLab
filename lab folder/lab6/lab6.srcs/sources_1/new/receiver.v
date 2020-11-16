ff4e02f62891f2519852b77482cf0393cd2abbc6
puttimeth <puttimeth.suppawawisit@gmail.com>
2020-11-16 21:22:16 +0700

    update lab6

---
+++ lab folder/lab6/lab6.srcs/sources_1/new/receiver.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/16/2020 08:09:01 PM
+// Design Name: 
+// Module Name: receiver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module receiver(
+
+    );
+endmodule
