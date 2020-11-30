5836974ace02599b9cfbe1b2bf62b115a4302148
puttimeth <puttimeth.suppawawisit@gmail.com>
2020-11-30 10:33:22 +0700

    update project

--- Term_Project/Term_Project.srcs/sources_1/imports/new/uartSystem.v
+++ Term_Project/Term_Project.srcs/sources_1/imports/new/uartSystem.v
@@ -24,15 +24,21 @@ module uartSystem(
     output RsTx
     );
     
-    reg [7:0]K_UP = 8'h77;            //w
-    reg [7:0]K_DOWN = 8'h73;          //s
-    reg [7:0]K_RIGHT = 8'h64;         //d
-    reg [7:0]K_LEFT = 8'h61;          //a
-        
-    reg [7:0]K_WHITE = 8'h20;         //SPACE
-    reg [7:0]K_MAGENTA = 8'h6d;       //m
-    reg [7:0]K_CYAN = 8'h63;          //c
-    reg [7:0]K_YELLOW = 8'h79;        //y
+    reg [7:0] K0 = 8'h30;       //0
+    reg [7:0] K1 = 8'h31;       //1
+    reg [7:0] K2 = 8'h32;       //2
+    reg [7:0] K3 = 8'h33;       //3
+    reg [7:0] K4 = 8'h34;       //4
+    reg [7:0] K5 = 8'h35;       //5
+    reg [7:0] K6 = 8'h36;       //6
+    reg [7:0] K7 = 8'h37;       //7
+    reg [7:0] K8 = 8'h38;       //8
+    reg [7:0] K9 = 8'h39;       //9
+    reg [7:0] K_PLUS = 8'h2b;       //+
+    reg [7:0] K_MINUS = 8'h2d;      //-
+    reg [7:0] K_MUL = 8'h2a;        //*
+    reg [7:0] K_DIV = 8'h2f;        ///
+    reg [7:0] K_ENTER = 8'h0a;      //new line
     
     reg ena, last_rec;
     reg [7:0] data_in;
@@ -45,26 +51,30 @@ module uartSystem(
     receiver receiver(baud, RsRx, received, data_out);
     transmitter transmitter(baud, data_in, ena, sent, RsTx);
     
-    assign data_out_plus = data_out + 1;
-
     always @(posedge baud)
     begin
         if (ena == 1) ena = 0;
         
         if (~last_rec & received) begin
-            /*case (data_out)
-                K_UP : data_in = 8'h57;
-                K_DOWN : data_in = 8'h53;
-                K_RIGHT : data_in = 8'h44;
-                K_LEFT : data_in = 8'h41;
-                K_WHITE : data_in = 8'h5A;
-                K_MAGENTA : data_in = 8'h4D;
-                K_CYAN : data_in = 8'h43;
-                K_YELLOW : data_in = 8'h59;
-                default : data_in = 8'hFF;
-            endcase*/
+            case (data_out)
+                K0: begin data_in = data_out; end
+                K1: begin data_in = data_out; end
+                K2: begin data_in = data_out; end
+                K3: begin data_in = data_out; end
+                K4: begin data_in = data_out; end
+                K5: begin data_in = data_out; end
+                K6: begin data_in = data_out; end
+                K7: begin data_in = data_out; end
+                K8: begin data_in = data_out; end
+                K9: begin data_in = data_out; end                
+                K_PLUS: begin data_in = data_out; end
+                K_MINUS: begin data_in = data_out; end
+                K_MUL: begin data_in = data_out; end
+                K_DIV: begin data_in = data_out; end
+                K_ENTER: begin data_in = data_out; end
+                default: data_in = 8'hff;
+            endcase
             if (data_in != 8'hFF) ena = 1;
-            data_in = data_out_plus;
         end
         last_rec = received;
     end
